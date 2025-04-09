ae41b285ef5f9f5851db17b0a6683403b096abe1
Jonas Wahle <wahle.jonas@web.de>
2025-04-09 13:25:05 +0200

    add interface for parser

---
+++ GenesisApi/Interfaces/ITableParser.cs
@@ -0,0 +1,9 @@
+﻿using GenesisApi.Models;
+
+namespace GenesisApi.Interfaces
+{
+    public interface ITableParser
+    {
+        List<ParsedTableRow> ParseTableContent(string rawContent);
+    }
+}
