e4c9cc1217b671dc7f61744a312cca961473a6a6
Jonas Wahle <wahle.jonas@web.de>
2025-04-09 13:30:19 +0200

    Refactoring

--- GenesisApi/Controllers/GenesisController.cs
+++ GenesisApi/Controllers/GenesisController.cs
@@ -1,35 +1,43 @@
-﻿using GenesisApi.Models;
+﻿using GenesisApi.Interfaces;
+using GenesisApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
 
 namespace GenesisApi.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class GenesisController : Controller
+    [Route("api/genesis")]
+    public class GenesisController(IHttpClientFactory _httpClientFactory, ITableParserService _tableParserService) : Controller
     {
-        private readonly HttpClient _httpClient;
+        private readonly HttpClient _httpClient = _httpClientFactory.CreateClient();
 
         public GenesisController(IHttpClientFactory httpClientFactory)
         {
             _httpClient = httpClientFactory.CreateClient();
         }
 
-        [HttpPost("logincheck")]
+        [HttpPost("check-login")]
         public async Task<IActionResult> LoginCheck([FromBody] LoginRequest request)
         {
-            var values = new Dictionary<string, string>
+            Dictionary<string, string> values = new()
         {
             { "username", request.Username },
             { "language", request.Language }
         };
 
             if (!string.IsNullOrWhiteSpace(request.Password))
+            {
                 values.Add("password", request.Password);
+            }
 
-            var content = new FormUrlEncodedContent(values);
+            FormUrlEncodedContent content = new(values);
 
-            var response = await _httpClient.PostAsync("https://www-genesis.destatis.de/genesisWS/rest/2020/helloworld/logincheck", content);
-            var responseBody = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage responseMessage = await _httpClient.PostAsync(
+                "https://www-genesis.destatis.de/genesisWS/rest/2020/helloworld/logincheck", 
+                content);
+
+            string responseBody = await responseMessage.Content.ReadAsStringAsync();
 
             return Content(responseBody, "application/json");
         }
