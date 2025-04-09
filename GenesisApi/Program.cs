8e6099c0969b84d665bac94eabeddecbfdd2f37c
Jonas Wahle <wahle.jonas@web.de>
2025-04-09 13:27:43 +0200

    Add services to DI

--- GenesisApi/Program.cs
+++ GenesisApi/Program.cs
@@ -1,18 +1,31 @@
+using GenesisApi.Interfaces;
+using GenesisApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services
+builder.Services.AddScoped<ITableParserService, TableParserService>();
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
-var app = builder.Build();
+WebApplication webApp = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+webApp.MapControllers();
+webApp.UseHttpsRedirection();
+webApp.UseAuthorization();
+webApp.UseSwagger();
+webApp.UseSwaggerUI();
+webApp.UseCors("AllowAll");
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.Run();
+webApp.Run();
