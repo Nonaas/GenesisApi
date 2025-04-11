56c41703e48bb2a98bef54e94b9b39e4689c0a4b
Jonas Wahle <wahle.jonas@web.de>
2025-04-11 14:48:04 +0200

    Add method extensions

---
+++ GenesisApi/Utilities/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace GenesisApi.Utilities
+{
+    public class Extensions
+    {
+        public static double? ParseDouble(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value == ".")
+            {
+                return null;
+            }
+            else if (double.TryParse(value.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
